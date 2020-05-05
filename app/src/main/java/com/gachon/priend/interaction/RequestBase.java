523c3d7e8271d35cab503d63be5a0dca0626782f
nda111 <48427099+nda111@users.noreply.github.com>
2020-05-05 23:56:10 +0900

    Create RequestBase.java

---
+++ app/src/main/java/com/gachon/priend/interaction/RequestBase.java
@@ -0,0 +1,88 @@
+package com.gachon.priend.interaction;
+
+/**
+ * A abstract class that represents request-response routine
+ *
+ * @param <T> The type of the response
+ * @author 유근혁
+ * @since May 5th 2020
+ */
+public abstract class RequestBase<T> {
+
+    /**
+     * A callback that handles response
+     *
+     * @param <T> The type of the response
+     * @author 유근혁
+     * @since May 5th 2020
+     */
+    public interface ResponseListener<T> {
+
+        /**
+         * Called on routine terminated
+         *
+         * @param response The response value
+         * @param args Additional response arguments which is optional
+         */
+        void onResponse(T response, Object[] args);
+    }
+
+    /**
+     * Response of this request
+     */
+    protected T response = null;
+
+    /**
+     * Get the URI of web socket server
+     *
+     * @return URI of web socket server
+     */
+    protected abstract String getUri();
+
+    /**
+     * Called to send request arguments
+     *
+     * @param conn A connection interface
+     */
+    protected abstract void onRequest(WebSocketRequest conn);
+
+    /**
+     * Method that handles response, must set value of {super.response}
+     *
+     * @param conn A connection interface
+     * @param message A response message
+     * @param paramNumber A sequence number of received message that starts with 0
+     */
+    protected abstract void onResponse(WebSocketRequest conn, WebSocketRequest.Message message, int paramNumber);
+
+    /**
+     * Clean up
+     */
+    protected abstract void onClose();
+
+    /**
+     * Send this request and call {responseListener.onResponse}
+     *
+     * @param responseListener A callback that handles response
+     */
+    public final void request(final ResponseListener<T> responseListener) {
+        new WebSocketRequest(getUri(), new WebSocketRequest.RequestAdapter() {
+
+            @Override
+            public void onRequest(WebSocketRequest conn) {
+                RequestBase.this.onRequest(conn);
+            }
+
+            @Override
+            public void onResponse(WebSocketRequest conn, WebSocketRequest.Message message, int paramNumber) {
+                RequestBase.this.onResponse(conn, message, paramNumber);
+            }
+
+            @Override
+            public void onClosed() {
+                RequestBase.this.onClose();
+                responseListener.onResponse(response, null);
+            }
+        }).connect(null);
+    }
+}
