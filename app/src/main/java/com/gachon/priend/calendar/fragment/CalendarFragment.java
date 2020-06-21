5fa1dd94412443382d6a170c8d3215b41a4d38e7
nda111 <48427099+nda111@users.noreply.github.com>
2020-06-22 04:32:46 +0900

    Update CalendarFragment.java

--- app/src/main/java/com/gachon/priend/calendar/fragment/CalendarFragment.java
+++ app/src/main/java/com/gachon/priend/calendar/fragment/CalendarFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,31 +26,17 @@ import java.util.Calendar;
  */
 public class CalendarFragment extends Fragment {
 
-    private static final String[] Days = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-    private static final String[] Months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-
     private Date date = null;
 
     private MaterialTextView yearTextView = null;
     private MaterialTextView dateTextView = null;
     private CalendarView calendarView = null;
 
-    private CalendarView.OnDateChangeListener onDateChangeListener = null;
-
-    private String generateDateText() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(date.toMillis());
-
-        return Days[calendar.get(Calendar.DAY_OF_WEEK)] + ", " +
-                Months[calendar.get(Calendar.MONTH)] + ' ' +
-                calendar.get(Calendar.DAY_OF_MONTH);
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setDate(Date.getNow());
+        this.date = Date.getNow();
     }
 
     @Override
@@ -66,7 +53,6 @@ public class CalendarFragment extends Fragment {
                 setDate(new Date(year, month + 1, dayOfMonth));
             }
         });
-
         setDate(date);
 
         return view;
@@ -87,9 +73,12 @@ public class CalendarFragment extends Fragment {
      */
     public void setDate(@NonNull Date date) {
         this.date = date;
+
         calendarView.setDate(date.toMillis());
 
-        yearTextView.setText(date.getYear());
-        dateTextView.setText(generateDateText());
+        yearTextView.setText(Integer.toString(date.getYear()));
+        dateTextView.setText(this.date.toString(getResources().getConfiguration().getLocales().get(0)));
+
+        Log.d("CalendarSelectionActivity", date.toString(getResources().getConfiguration().getLocales().get(0)));
     }
 }
