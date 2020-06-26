6a37fcda0a11b5ee2a334b41ff0925e991dc91e0
Jian <luckywon88@gmail.com>
2020-06-26 13:25:35 +0900

    Editing Post(include Delete)
    Editing Post(include Delete)

---
+++ app/src/main/java/com/gachon/priend/community/database/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.gachon.priend.community.database;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static String NAME = "post.db";
+    public static int VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table if not exists noteData("
+                + " _id integer PRIMARY KEY autoincrement, "
+                + " title text, "
+                + " content text, "
+                + " time text)";
+        db.execSQL(sql);
+    }
+
+    public void onOpen(SQLiteDatabase db) {    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion > 1) {
+            db.execSQL("DROP TABLE IF EXISTS noteData");
+        }
+    }
+}
