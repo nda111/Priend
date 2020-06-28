3f69503ede4eef1b50d79a35f9976f1e5d9c0b19
nda111 <48427099+nda111@users.noreply.github.com>
2020-06-28 15:28:37 +0900

    Update SettingsActivity.java

--- app/src/main/java/com/gachon/priend/settings/activity/SettingsActivity.java
+++ app/src/main/java/com/gachon/priend/settings/activity/SettingsActivity.java
@@ -14,5 +14,21 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.settings_container_item_account).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(SettingsActivity.this, AccountSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.settings_container_item_alert).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(SettingsActivity.this, AlertSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
